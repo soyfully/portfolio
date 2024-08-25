8ba355d499add55d165b113d066d040b09edc373
soyfully <soyfully@gmail.com>
2024-08-25 15:32:43 +0900

    [fix] styled component 추가 및 creat react app 으로 생성된 파일 중 사용하지 않는 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          soi
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrap>
+      TEST
+    </Wrap>
   );
 }
+
+const Wrap = styled.div`
+  color: #222;
+`
 
 export default App;
